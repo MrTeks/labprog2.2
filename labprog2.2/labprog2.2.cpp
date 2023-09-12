13c494087fb010ffbd37aa039a80ca6035e9ba1c
Teks <Teks@TEKS>
2023-09-12 17:58:23 +0700

    Добавьте файлы проекта.

---
+++ labprog2.2/labprog2.2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    int a, b, c;
+
+    cout << "Введите число: ";
+    cin >> a;
+
+    c = a % 10;
+    a /= 10;
+    b = (c * 10) + a;
+
+    cout << "Обратное число: " << b << endl;
+}
